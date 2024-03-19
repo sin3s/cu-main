ac4f965033d3424b5c962f7a519c3fe2d6abf2a2
sin3s <159211135+sin3s@users.noreply.github.com>
2024-03-19 18:29:15 +0800

    Add files via upload

--- js/cu-work.js
+++ js/cu-work.js
@@ -1,19 +1,17 @@
 window.addEventListener('scroll', function() {
-    var scrollPosition = window.scrollY;
-    var cards = document.querySelectorAll('.cu-work-card');
+  var scrollPosition = window.scrollY;
+  var cards = document.querySelectorAll('.cu-work-card');
+
+  for (var i = 0; i < cards.length; i++) {
+      var cardPosition = cards[i].getBoundingClientRect().top;
+      var screenPosition = window.innerHeight / 2;
+
+      if(cardPosition < screenPosition) {
+          cards[i].classList.add('cu-work-card-appear');
+      } else {
+          cards[i].classList.remove('cu-work-card-appear');
+      }
+  }
+});
   
-    for (var i = 0; i < cards.length; i++) {
-        // Exclude the card with id 'cu-work-card-1'
-        if (cards[i].id === 'cu-work-card-1') continue;
-  
-        var cardPosition = cards[i].getBoundingClientRect().top;
-        var screenPosition = window.innerHeight / 2;
-  
-        if(cardPosition < screenPosition) {
-            cards[i].classList.add('cu-work-card-appear');
-        } else {
-            cards[i].classList.remove('cu-work-card-appear');
-        }
-    }
-  });
-  
+
