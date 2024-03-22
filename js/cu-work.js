00582f76b67a824c1a4007ceb0640c10245f49d3
sin3s <159211135+sin3s@users.noreply.github.com>
2024-03-22 15:41:55 +0800

    Add files via upload

--- js/cu-work.js
+++ js/cu-work.js
@@ -14,5 +14,17 @@ window.addEventListener('scroll', function() {
   }
 });
 
+$(window).on('load', function() {
+    $('#preloader').css({
+      "transform": "translateY(-100%)",
+      "transition-delay": "0.6s"
+    });
+    $('.loader').css({
+      "opacity": "0",
+      "transform": "translate(-50%,-100%)",
+      "transition-delay": "0.3s"
+    });
+  });
+
 
   
