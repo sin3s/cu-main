8e6915023ba4094207ecf28ba6c614d059311fd5
sin3s <159211135+sin3s@users.noreply.github.com>
2024-03-25 15:17:41 +0800

    Add files via upload

--- js/cu-work.js
+++ js/cu-work.js
@@ -26,34 +26,23 @@ $(window).on('load', function() {
   });
 });
 
-// Get the loader bar element
 var loaderBar = document.getElementById('loader_bar');
-
-// Initialize the loader bar width
 var loaderWidth = 0;
 
-// Function to update the loader bar width
 function updateLoaderWidth() {
-  // Calculate a random increase in width
   var increase = Math.random() * 5;
-
-  // Calculate the new width
   loaderWidth += increase;
 
-  // If the new width is greater than 87%, stop the interval
   if (loaderWidth >= 87) {
     loaderWidth = 87;
     clearInterval(interval);
   }
 
-  // Update the width of the loader bar
   loaderBar.style.setProperty('--loader-width', loaderWidth + '%');
 }
 
-// Start an interval to increase the loader bar width
 var interval = setInterval(updateLoaderWidth, 200);
 
-// When the window is fully loaded, immediately set the width to 100%
 window.onload = function () {
   clearInterval(interval);
   loaderBar.style.setProperty('--loader-width', '100%');
